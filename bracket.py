22e91c89450101e7608219fc1d8fd6d1d3792bb3
amit <a.k.virmani@gmail.com>
2017-03-03 08:47:03 -0500

    match multiple types of brackets

--- bracket.py
+++ bracket.py
@@ -1,19 +1,29 @@
 def balanced_brackets(s):
     l=[]
-
+    balanced=True
     for c in s:
-        if c == '(':
+        if c in '({[':
             l.append(c)
         else:
             if l:
-                l.pop()
+                top=l.pop()
+                if not matches(top,c):
+                    balanced = False
             else:
-                return False
+                balanced = False
 
-    return not l
+    if balanced and not l:
+        return True
+    else:
+        return False
+
+def matches(open, close):
+    open_list="({["
+    close_list=")}]"
+    return open_list.index(open) == close_list.index(close)
 
 if __name__ == '__main__':
-    if balanced_brackets('((())'):
+    if balanced_brackets('({[]})'):
         print 'Brackets are balanced'
     else:
         print 'Unbalanced bracktes'
