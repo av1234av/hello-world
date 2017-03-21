216764cc3b84caba887775ac6cde88e813643ee7
amit <a.k.virmani@gmail.com>
2017-03-21 08:31:44 -0400

    improved

--- duplicates.py
+++ duplicates.py
@@ -3,7 +3,7 @@ def remove_dupes(ll):
     while i < len(ll):
         x=ll[i+1:].index(ll[i])
         if x:
-            ll.remove(ll[x-1])
+            ll.remove(ll[x+1])
         # if ll[i] in ll[i+1:]:
         #     ll[i+1:].remove(ll[i])
         # else:
@@ -12,14 +12,17 @@ def remove_dupes(ll):
     return ll
 
 def remove_dupes1(ll):
-    for idx, item in enumerate(ll):
-        if len(ll) <= idx: break
-        if item in ll[:idx]:
-            ll.pop(idx)
-
-    return ll
+    new_ll=[]
+    for i in range(len(ll)):
+        if ll[i] in ll[i+1:]:
+            continue
+        new_ll.append(ll[i])
+    return new_ll
 
 if __name__ == '__main__':
 
-    print remove_dupes([2,3,4,5,1,5,2,2,4,2,3])
-    print remove_dupes1([2, 3, 4, 5, 1, 5, 2, 2, 4, 2, 3])
+    # print remove_dupes([2,3,4,5,1,5,2,2,4,2,3])
+    ll=[2, 3, 4, 5, 1, 5, 2, 2, 4, 2, 3]
+
+    print [ll[i] for i in range(len(ll)) if ll[i] not in ll[i+1:]]
+    print remove_dupes1(ll)
